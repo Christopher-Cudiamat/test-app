2af33a7c4d7d0ab7036f6992b814c28dd3f6a47f
Christopher Cudiamat <christophervcudiamat.13@gmail.com>
2021-05-12 08:32:47 +0800

    Work-Sans font set up with font-face

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.style.scss";
 
 const App: React.FC = () => {
-  return <div className="App">INITIAL SET UP</div>;
+  return <div className="App">Almost</div>;
 };
 
 export default App;
